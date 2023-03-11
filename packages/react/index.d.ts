bc3c3fed3e2b4d85b419015da765810e01bfdbbe
Fabien JUIF <fabien.juif@gmail.com>
2023-03-11 16:33:08 +0100

    :label: fix react ts declarations

--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -1,13 +1,19 @@
 import * as React from "react";
-import { Action, Store } from "@myrtille/core";
+import { Action, Store, Reaction } from "@myrtille/core";
 
-export declare function provider<STATE, STORE extends Store = Store<STATE>>(
+export declare function provider<
+  STATE,
+  STORE extends Store<STATE> = Store<STATE>
+>(
   store: STORE
 ): (
   Component: any
 ) => (props: any) => React.FunctionComponentElement<React.ProviderProps<any>>;
 
-export declare const Provider: <STATE, STORE extends Store = Store<STATE>>({
+export declare const Provider: <
+  STATE,
+  STORE extends Store<STATE> = Store<STATE>
+>({
   store,
   children,
 }: {
@@ -17,11 +23,13 @@ export declare const Provider: <STATE, STORE extends Store = Store<STATE>>({
 
 export declare function useStore<
   STATE,
-  STORE extends Store = Store<STATE>
+  STORE extends Store<STATE> = Store<STATE>
 >(): STORE;
 
 export declare function useDispatch(): (action: Action | string) => void;
 
-export declare function useListeners(): (listeners: Reaction[]) => () => void;
+export declare function useListeners<STORE>(
+  listeners: Reaction<STORE>[]
+): () => void;
 
 export declare function useStateAt<STATE>(path?: string): STATE;
